bccb9d49e359667c00f0522939ffaa13cf71e498
Franck Panou <85650372+FPan7792@users.noreply.github.com>
2023-07-23 02:07:28 +0200

    clean up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
-// import { router } from "./router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <RouterProvider router={router} /> */}
     <App />
   </React.StrictMode>
 );
